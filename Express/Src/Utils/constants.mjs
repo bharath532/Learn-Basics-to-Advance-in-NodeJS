37dafb15ce949b4b7cc69c93b6482ec3a7b934b3
BHARATH <bharath978930@gmail.com>
2026-04-24 11:50:47 +0530

    Express

--- Express/Src/Utils/constants.mjs
+++ Express/Src/Utils/constants.mjs
@@ -1,11 +1,12 @@
 export  const users=[
-     {id:1,user_name:"Alan"},
-    {id:2,user_name:"John"},
-    {id:3,user_name:"Finch"},
-    {id:4,user_name:"Andrew"},
-    {id:5,user_name:"HulK"},
-    {id:6,user_name:"Iron Man"}
+     {id:1,user_name:"Alan",password:"09876"},
+    {id:2,user_name:"John",password:"5432"},
+    {id:3,user_name:"Finch", password:"1234"},
+    {id:4,user_name:"Andrew",password:"5678"},
+    {id:5,user_name:"HulK",password:"901"},
+    {id:6,user_name:"Iron Man",password:"2345"}
 ]
+
 
 export const products=[
     {id:1,p_name:"Phone"},
--- Express/Src/index.mjs
+++ Express/Src/index.mjs
@@ -5,6 +5,9 @@ import express from 'express';
 import Routes from './Routes/Routes.mjs';
 import cookieparser from 'cookie-parser';
 import session from 'express-session';
+import {Strategy as Localstrategy} from 'passport-local';
+import passport from "passport";
+import { users } from './Utils/constants.mjs';
 const App = express();
 
 const Port = 3000;
@@ -21,6 +24,33 @@ App.use(session(
         }
     }
 ))
+App.use(passport.initialize());
+App.use(passport.session());
+
+passport.use( new Localstrategy(
+    {usernameField:"user_name",passwordField:"password"}
+    ,(user_name,password,done)=>{
+    const user=users.find((user)=>user_name==user_name);
+    if(!user){
+        return done(null, false,{message:"invalid username"});
+    }
+
+    if(user.password!==password){
+        return done(null ,false,{message:"incorrect password"})
+    }
+    return done(null,user)
+}))
+
+passport.serializeUser((user, done)=>{
+    done(null,user.id)
+})
+
+passport.deserializeUser((id, done)=>{
+    const user =users.find((u)=>u.id==id)
+
+    done(null,user||false);
+})
+
 App.use(Routes);
 
 // GET
@@ -40,6 +70,23 @@ App.get('/',(req,res)=>{
     res.send({msg:"root"})
 })
 
+
+
+App.post("/login",(req,res,next)=>{
+    passport.authenticate("local",(err,user,info)=>{
+        if(err) return next(err);
+        if(!user){
+            return res.status(401).json({message: info?.message ||"User Not Found"})
+        }
+
+        req.logIn(user,err=>{
+            if(err) return next(err);
+            return res.json({message:'Login Done',user})
+        })
+
+
+    })(req,res,next);
+})
 
 
 App.listen(Port,()=>{
